db11a3e4d6e736eb6fb042e01b8dbc3fa5947434
TomerCatabi <tomercatabi@mail.tau.ac.il>
2024-08-02 12:18:34 +0300

    Update plotter_gary.m
    commenting

--- plotter_gary.m
+++ plotter_gary.m
@@ -1,7 +1,10 @@
+%filename should be edited according to the video name that were saved on your local computer
 v= VideoReader('2024-07-14 01-22-17.mp4');
+%Final value of i shoud be changed to the number of frames in the video (30 frames in a second -> 30*number_of_seconds)
 for i=1:1:30*134
 frame = read(v,i);
 
+%D2 sums on the specific coordinates given (yi:yf, xi:xf)
 %pcolor(frame(400:633,910:1220,1))
 D2(i)=sum(sum(frame(201:242,1022:1124,1)));
 %D2(i)=sum(sum(frame(406:647,461:785,1)));
@@ -19,6 +22,6 @@ end
 
 plot(D2) 
 
-
+% one can uncomment the following lines in order to get a matlab image of the firs frame and extruct the y and x coordinates given
 %pcolor(frame(:,:,1))
 %shading flat
