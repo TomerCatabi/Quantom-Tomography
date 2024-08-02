bd71e167ffa9035428b62c66f068302fbe122d58
TomerCatabi <tomercatabi@mail.tau.ac.il>
2024-08-02 12:43:08 +0300

    Update plotter_gary_for_part_c.m
    Adding comments

--- plotter_gary_for_part_c.m
+++ plotter_gary_for_part_c.m
@@ -1,6 +1,10 @@
+% videoFileName should be changed according to the way your file was saved
 videoFileName = '+90+112.5.mp4';
 v= VideoReader(videoFileName);
+% Extructing the number of frames
 NumFrames = v.NumFrames;
+% Each one of the coordinates varibles holdes [yi,yf,xi,xf] for each of the cameras on the screen of the video
+% Coordingates corrisponds to x and y coordinates that were found in our expirment and should be chanced accordingly for different video
 coordinates_A = [176,214,1005,1133];
 coordinates_B = [230,260,647,704];
 for i=1:1:NumFrames
@@ -26,11 +30,14 @@ end
 plot(D2_A)
 plot(D2_B)
 
+% saving the matrices in a csv file with corisponding name to the angles that were measured
+% This of cours take in to account using the same name format as was used in line 2
 FileName = videoFileName(1:end-4);
 FileName = strcat(FileName,'.csv');
 writematrix(D2_A,FileName);
 writematrix(D2_B,FileName,'WriteMode','append');
 
 
+% one can uncomment the following lines in order to get a matlab image of the firs frame and extruct the y and x coordinates given
 %pcolor(frame(:,:,1))
 %shading flat
